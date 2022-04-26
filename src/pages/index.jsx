ddbc159f5dbfe0e2422800a59fb25870c1d42767
idahpews <idatheresetroan@gmail.com>
2022-04-26 15:16:58 +0200

    pathing sucks for some reason

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { useProjectData } from '../hooks/useProjectData';
-import Layout from '../components/Layout/Layout';
+import Layout from '../components/Layout';
 
 // markup
 const HomePage = () => {
